0cb39d25990ded3d5571ec970a0d2e8d7caaf2c8
Prashant <mediapka@gmail.com>
2025-11-19 14:17:02 +0530

    init checkin

---
+++ APPS/reactwebappdemo/src/components/AboutUs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./Header"; 
+
+export const AboutUs: React.FC = () => {
+    return (         
+        <div><Header />
+            <div>This is the About Us screen for React demo developed by Prashant Akhouri</div>
+            </div>
+    ); 
+}
+
+export default AboutUs;
